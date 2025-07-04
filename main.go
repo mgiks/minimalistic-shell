c88016fb8bb63f2afdf33249a72608348ea2d13a
mgiks <lidamil6785@gmail.com>
2025-07-04 23:39:02 +0600

    feat: add basic functionality of 'cd'

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
 	"os/exec"
@@ -28,6 +29,15 @@ func execInput(input string) error {
 	input = strings.TrimSuffix(input, "\n")
 
 	args := strings.Split(input, " ")
+
+	switch args[0] {
+	case "cd":
+		if len(args) < 2 {
+			return errors.New("path required")
+		}
+
+		return os.Chdir(args[1])
+	}
 
 	cmd := exec.Command(args[0], args[1:]...)
 
